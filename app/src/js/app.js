0386d26f61adafb24aca752d258b5e296cd0a7d0
John Murray <heyjohnmurray@us.ibm.com>
2015-11-29 01:24:53 -0600

    initial commit. plan my logic

---
+++ app/src/js/app.js
@@ -0,0 +1,4 @@
+// should be some sort of counting logic
+// setTimeOut to always update the seconds/minutes/hours
+// function to write updated time to the DOM
+// find a way to add a description to and save timed sessions
