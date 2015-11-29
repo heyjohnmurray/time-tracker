6a607c9ce64f762b40521d560e0d23aa83d3e620
John Murray <heyjohnmurray@us.ibm.com>
2015-11-29 13:35:05 -0600

    going to write it all procedural first

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,33 +1,21 @@
+// timer object
 // should be some sort of counting logic
 // setTimeOut to always update the seconds/minutes/hours
 // function to write updated time to the DOM
 // find a way to add a description to and save timed sessions
 
-(function(undefined){
-  var playBtn = document.querySelector('.js-play');
-  var pauseBtn = document.querySelector('.js-pause');
-  var stopBtn = document.querySelector('.js-stop');
+var playBtn = document.querySelector('.js-play');
+var pauseBtn = document.querySelector('.js-pause');
+var stopBtn = document.querySelector('.js-stop');
 
-  playBtn.addEventListener('click', function(e) {
-    console.log('play');
-  });
+playBtn.addEventListener('click', function(e) {
+  console.log('play');
+});
 
-  pauseBtn.addEventListener('click', function(e) {
-    console.log('pause');
-  });
+pauseBtn.addEventListener('click', function(e) {
+  console.log('pause');
+});
 
-  stopBtn.addEventListener('click', function(e) {
-    console.log('stop');
-  });
-
-  function TrackerController() {
-    console.log('tracker controller');
-  }
-
-  function TrackerUI() {
-    this.TrackerController = new TrackerController();
-    console.log('tracker UI');
-  }
-
-  var timeTracker = new TrackerUI();
-})();
+stopBtn.addEventListener('click', function(e) {
+  console.log('stop');
+});
