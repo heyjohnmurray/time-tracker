35be0223692cc3650c89d75c01fdda66e4bbeadb
John Murray <heyjohnmurray@us.ibm.com>
2015-11-30 17:01:01 -0600

    git leading zeroes working

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -24,11 +24,7 @@ function timing() {
     minutes = 0;
   }
 
-  if (seconds < 9) {
-    seconds = '0' + seconds;
-  }
-
-  timerOutput.innerHTML = hours + ' : ' + minutes + ' : ' + seconds;
+  timerOutput.innerHTML = (hours ? (hours > 9 ? hours : '0' + hours) : '00') + ':' + (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':' + (seconds > 9 ? seconds : '0' + seconds);
 }
 
 function timer() {
